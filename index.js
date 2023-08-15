9120792705fbd1f49373c055e500d8ba7a9f8574
Cristal Davila <cristal.davila@gmail.com>
2023-08-15 12:35:45 -0600

    displays images, populates data, filter in progress

--- index.js
+++ index.js
@@ -14,15 +14,29 @@ function renderActivity(activity) {
     uniqueCategories.push(activity.category);
     
     const activityCategoryElement = document.createElement('div');
-    activityCategoryElement.textContent = activity.category;
+    activityCategoryElement.className = 'spacing'
+    activityCategoryElement.textContent = '' + `${activity.category}` + '';
     activityDiv.append(activityCategoryElement);
 
+    
+    //when I click on a category I want an image for that category and a list of activities under that category to populate in the detailed-info section of the page.
+   
     activityCategoryElement.addEventListener('click',()=>{
-     display.nameElement.textContent = activities.name
-    })
+        displayActivityInfo(activity)
         
-    }
-  }
+function displayActivityInfo(activities){
+    
+console.log(activities)
+     const activityNameElement =document.getElementById("activity-name");
+     const imageElement = document.querySelector("#category-display-image");
+     const descriptionElement = document.getElementById("activity-description");
 
+     activityNameElement.textContent= activities.name
+    imageElement.src = activities.image
+     descriptionElement.textContent = activities.description
+   }
+}
+    )}}
 
-
+    function matchName(activities, category) {
+        return activities.filter(function (category) { return category.textContent === restaurant})}
