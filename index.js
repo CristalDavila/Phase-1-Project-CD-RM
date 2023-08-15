d007715f7668931556fdd682a25abe0f0debb3d9
Ryan Meyers <rhynogolfer42@gmail.com>
2023-08-15 09:28:43 -0600

    Updating file

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-fetch ("http://localhost:3000/activities")
-.then(res => res.json())
-.then(renderActivities);
+const activityDiv = document.getElementById('activities-bar');
+const uniqueCategories = [];
+
+fetch("http://localhost:3000/activities")
+  .then(res => res.json())
+  .then(renderActivities);
 
 function renderActivities(activities) {
-    activities.forEach(renderActivities);
+  activities.forEach(renderActivity);
+}
+
+function renderActivity(activity) {
+  if (uniqueCategories.indexOf(activity.category) === -1) {
+    uniqueCategories.push(activity.category);
+    
+    const activityCategoryElement = document.createElement('div');
+    activityCategoryElement.textContent = activity.category;
+    activityDiv.append(activityCategoryElement);
+  }
 }
