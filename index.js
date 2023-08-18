337ce5ff35a2a45522adf9fb8edf97db7489dccd
Cristal Davila <cristal.davila@gmail.com>
2023-08-18 11:28:42 -0600

    Merged

--- index.js
+++ index.js
@@ -6,36 +6,74 @@ fetch("http://localhost:3000/activities")
   .then(renderActivities);
 
 function renderActivities(activities) {
-  activities.forEach(renderActivity);
+  activities.forEach((activity) => renderActivity(activity, activities));
+
 }
 
-function renderActivity(activity) {
+function renderActivity(activity, activities) {
   if (uniqueCategories.indexOf(activity.category) === -1) {
     uniqueCategories.push(activity.category);
     const activityCategoryElement = document.createElement('div');
     activityCategoryElement.className = 'spacing'
-    activityCategoryElement.textContent = '' + `${activity.category}` + '';
+    activityCategoryElement.textContent = activity.category;
     activityDiv.append(activityCategoryElement);
 
-    
-    //when I click on a category I want an image for that category and a list of activities under that category to populate in the detailed-info section of the page.
-   
-    activityCategoryElement.addEventListener('click',()=>{
-        displayActivityInfo(activity)
-        
-function displayActivityInfo(activities){
-    
-console.log(activities)
-     const activityNameElement =document.getElementById("activity-name");
-     const imageElement = document.querySelector("#category-display-image");
-     const descriptionElement = document.getElementById("activity-description");
+    activityCategoryElement.addEventListener('click', () => {
+      console.log(activities);
+      console.log(activity.category);
+      const detailedInfo = document.querySelector("#detailed-info")
+      detailedInfo.innerHTML = ""
 
-     activityNameElement.textContent= activities.name
-    imageElement.src = activities.image
-     descriptionElement.textContent = activities.description
-   }
+      const filteredActivities = activities.filter((data) => {
+        return (data.category === activity.category)
+      })
+      console.log(filteredActivities)
+
+      // foreach that breaks down the full category array into individual activities
+
+      filteredActivities.forEach(singleAct => {
+        displayActivityInfo(singleAct)
+      })
+    })
+  }
 }
-    )}}
 
-    function matchName(activities, category) {
-        return activities.filter(function (category) { return category.textContent === restaurant})}
+function displayActivityInfo(activity) {
+  console.log(activity)
+
+  const activityNameElement = document.createElement("div");
+  const imageElement = document.createElement("img");
+  const descriptionElement = document.createElement("div")
+  const orderedList = document.createElement("ul")
+
+  activityNameElement.textContent = activity.name
+  imageElement.src = activity.image
+  descriptionElement.textContent = activity.description
+  orderedList.append(imageElement, activityNameElement, descriptionElement)
+
+  // appends to the div detailed-info
+  const detailedInfo = document.querySelector("#detailed-info")
+  detailedInfo.append(orderedList);
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const dataOne = document.querySelector("#create-task-form")
+  dataOne.addEventListener("submit", (e) => {
+  e.preventDefault();
+  handleToDo(e.target["new-task-description"].value);
+  })
+})
+function handleToDo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo}`
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
